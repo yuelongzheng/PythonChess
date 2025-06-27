9de0b5892bcfc551fe9e66f5e819cd627c2cfe4c
yuelongzheng <132852557+yuelongzheng@users.noreply.github.com>
2025-06-27 18:33:51 +1000

    Added Undo function

--- ChessEngine.py
+++ ChessEngine.py
@@ -19,14 +19,57 @@ class GameState():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
-        self.whiteToMove = True
-        self.moveLog = []
+        self.white_to_move = True
+        self.move_log = []
     
     def make_move(self, move):
-        self.board[move.start_row][move.start_col] = '--'
+        self.board[move.start_row][move.start_col] = "--"
         self.board[move.end_row][move.end_col] = move.piece_moved
-        self.moveLog.append(move)
-        self.whiteToMove = not self.whiteToMove
+        self.move_log.append(move)
+        self.white_to_move = not self.white_to_move
+
+    def undo_move(self):
+        if len(self.move_log) >= 1 :
+            move = self.move_log.pop()
+            self.board[move.start_row][move.start_col] = move.piece_moved
+            self.board[move.end_row][move.end_col] = move.piece_captured
+            self.white_to_move = not self.white_to_move
+
+    '''
+    All moves considering checkmates
+    '''
+    def get_valid_moves(self):
+        return self.get_all_possible_moves()
+
+    '''
+    All moves regardless of checkmates
+    '''
+    def get_all_possible_moves(self):
+        moves = [Move((6,4), (4,4), self.board)]
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                piece = self.board[r][c][1]
+                if(turn == 'w' and self.white_to_move) and (turn == 'b' and not self.white_to_move):
+                    if piece == 'p':
+                        # self.get_pawn_moves(r, c, moves)
+                        pass
+                    elif piece == 'R':
+                        # self.get_rook_moves(r, c, moves)
+                        pass
+                    elif piece == 'N':
+                        # self.get_knight_moves(r, c, moves)
+                        pass
+                    elif piece == 'B':
+                        # self.get_bishop_moves(r, c, moves)
+                        pass
+                    elif piece == 'Q':
+                        # self.get_queen_moves(r, c, moves)
+                        pass
+                    elif piece == 'K':
+                        # self.get_king_moves(r, c, moves)
+                        pass
+        return moves
 
 class Move():
     ranks_to_rows = {}
@@ -46,6 +89,15 @@ class Move():
         self.end_col = end_square[1]
         self.piece_moved = board[self.start_row][self.start_col]
         self.piece_captured = board[self.end_row][self.end_col]
+        self.moveID = self.start_row * 1000 + self.start_col * 100 + self.end_row * 10 + self.end_col
+    
+    '''
+    Override equal method
+    '''
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
     
     def get_chess_notation(self):
         return self.get_rank_file(self.start_row, self.start_col) + self.get_rank_file(self.end_row, self.end_col)
--- ChessMain.py
+++ ChessMain.py
@@ -29,6 +29,8 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
+    valid_moves = gs.get_valid_moves()
+    move_made = False
     load_images()
     running = True
     square_selected = () # No square is selected, keep track of the last click of the user (tuple : (row, col))
@@ -51,11 +53,21 @@ def main():
                 if len(player_clicks) == 2:
                     move = ChessEngine.Move(player_clicks[0], player_clicks[1], gs.board)
                     # print(move.get_chess_notation())
-                    gs.make_move(move)
+                    if move in valid_moves:
+                        gs.make_move(move)
+                        move_made = True
                     # print(gs.board)
                     square_selected = ()
                     player_clicks = []
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z: # undo when the key 'z' is pressed
+                    gs.undo_move()
+                    move_made = True
 
+        if move_made:
+            valid_moves = gs.get_valid_moves()
+            move_made = False
+            
         draw_game_state(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
